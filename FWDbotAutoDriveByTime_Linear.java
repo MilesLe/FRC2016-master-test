de613bf02e3cc9317a29d2fa990a404b4b8c6de6
Miles Lee <mileslee@Miless-Air.lan>
2016-10-16 16:02:16 -0700

    test 2.2

--- FWDbotAutoDriveByTime_Linear.java
+++ FWDbotAutoDriveByTime_Linear.java
@@ -32,7 +32,7 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 package org.firstinspires.ftc.robotcontroller.external.samples;
 
-//Tester Push: 2.1
+//Tester Push: 2.2
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
